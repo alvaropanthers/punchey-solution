81a9002b264d4db9ef0e418fb669125898fbb332
Alvaro Orozco <alvaro.orozco.tech@gmail.com>
2020-06-03 20:48:59 -0400

    MADE A MISTAKE.. GOT CONFUSE WITH PRIME AND ALL ODD NUMBERS..

--- main.js
+++ main.js
@@ -8,14 +8,23 @@ const rl = readline.createInterface({
 let n
 rl.question('Enter n to find n-th prime number ', (answer) => {
   n = answer
-  console.log("Prime is " + nth(3))
+  console.log("Prime is " + nth_prime(n))
   rl.close()
 });
 
-function nth(n){
-  let last = 1
-  for(let i = 1; i < n * 2; i+=2)
-    last = i
+function nth_prime(n){
+  let primes = [2]
+
+  for(let current = 3; primes.length < n; current+=2){
+    let prime = true
+    for(let x = 0; x < primes.length; ++x)
+      if (current % primes[x] === 0){
+        prime = false
+        break
+      } 
+    if(prime) primes.push(current)
+
+    }
     
-  return last
+  return primes[primes.length - 1]
 }
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def nth_prime(n):
-    return range(1, n * 2, 2)[-1]
-
+    primes = [2]
+    odd = 3
+    while len(primes) < n:
+        for i in primes:
+            if odd % i == 0:
+                break
+        else:
+            primes.append(odd)
+        odd += 2
+    return primes[-1]
+    
 if __name__ == '__main__':
     n = int(input("Enter nth"))
     print("Prime is {v}".format(v=nth_prime(n)))
+
+#     for i in range(1, 60):
+#         print("{v} => {b}".format(v=i, b=nth_prime(i)))
